8eeecc5a1eb71c69cc2a9b30a04cf538e352ea3d
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2021-03-28 13:46:29 +0300

    create filtered logics

--- js/filtered-ads.js
+++ js/filtered-ads.js
@@ -1,7 +1,8 @@
 import {initMap} from './map.js';
 
 const ads = new Array;
-let filteredAds = new Array;
+
+// получаем данные
 
 export const getAds = (offers) => {
   for (let i = 0 ; i < offers.length ; i++) {
@@ -10,44 +11,197 @@ export const getAds = (offers) => {
   return ads;
 }
 
+// создаем общий массив
+
+const filteredMarkers = new Array;
+
+// пишем функцию которая инициализирует карты и передаем ей массив с метками
+
+const newMarkersInitMap = (filteredMarkers) => {
+
+  console.log(filteredMarkers);
+
+  for (let i = filteredMarkers.length -1 ; i > 0 ; i--) {
+    filteredMarkers.forEach((element) => {
+      if (filteredMarkers[i] == element) {
+        const indexOfEl = filteredMarkers.indexOf(filteredMarkers[i]);
+        filteredMarkers.splice(indexOfEl, 1);
+      }
+    })
+  }
+
+  console.log(filteredMarkers);
+
+  initMap(filteredMarkers);
+}
+
+// отлавливаем все события и меняем значение массива в одной функции
+
 const housingType = document.querySelector('#housing-type');
+const housingPrice = document.querySelector('#housing-price');
+const housingRooms = document.querySelector('#housing-rooms');
+const housingGuests = document.querySelector('#housing-guests');
+const housingFeatures = document.querySelector('#housing-features');
+const housingFeaturesList = housingFeatures.querySelectorAll('input');
+const checkedFeatures = () => {
+  const checkedFeaturesArray = new Array;
+  Array.from(housingFeaturesList).forEach((element) => {
+    if(element.checked == true) {
+      checkedFeaturesArray.push(element.value);
+    }
+  });
+  return checkedFeaturesArray;
+}
+
+let filteredAdsType = new Array;
+let filteredAdsPrice = new Array;
+// let filteredAdsRooms = new Array;
+// let filteredAdsGuests = new Array;
+// let filteredAdsFeatures = new Array;
+
+const markersData = (offers, filteredsArray) => {
+
+  getAds(offers).forEach((element) => {
+    filteredsArray.push(element);
+  });
+
+  for (let i = filteredsArray.length -1 ; i > 0 ; i--) {
+    getAds(offers).forEach((element) => {
+      if (filteredsArray[i] != element) {
+        const indexOfEl = filteredMarkers.indexOf(filteredMarkers[i]);
+        filteredMarkers.splice(indexOfEl, 1);
+      }
+    })
+  }
+  return filteredMarkers;
+}
+
+// type filters
+
+const pushFilteredElementType = (selectValue, filteredAdsType) => {
+
+  filteredAdsType.forEach((element) => {
+    if (selectValue === 'any') {
+      filteredAdsType.push(element);
+    }
+  })
+
+  if (selectValue === 'any') {
+    filteredAdsType.splice(0, filteredAdsType.length - 10);
+  } else {
+    for (let i = filteredAdsType.length - 1; i >= 0 ; i--) {
+      if (filteredAdsType[i].offer.type != selectValue) {
+        const indexOfEl = filteredAdsType.indexOf(filteredAdsType[i]);
+        filteredAdsType.splice(indexOfEl, 1);
+      }
+    }
+  }
+
+  filteredAdsType.forEach((element) => {
+    filteredMarkers.push(element);
+  })
+
+  return filteredMarkers;
+};
 
 housingType.addEventListener('change', (offers) => {
 
-  filteredAds = [];
+  // push data element markers
 
-  getAds(offers).forEach((element) => {
-    if (element.offer.type === housingType.value) {
-      filteredAds.push(element);
-    } else if (housingType.value === 'any') {
-      filteredAds.push(element);
-    }
-    return filteredAds;
-  })
+  let selectValue = housingType.value;
 
-  initMap(filteredAds);
+  if (filteredAdsType.length > 0) {
+    markersData(offers, filteredAdsType);
+    filteredMarkers.forEach((element) => {
+      filteredAdsType.push(element);
+    });
+  } else {
+    markersData(offers, filteredAdsType);
+  }
+
+  for (let i = filteredAdsType.length -1 ; i > 0 ; i--) {
+    filteredAdsType.forEach((element) => {
+      if (filteredAdsType[i] != element) {
+        const indexOfEl = filteredMarkers.indexOf(filteredMarkers[i]);
+        filteredMarkers.splice(indexOfEl, 1);
+      }
+    })
+  }
+
+  pushFilteredElementType(selectValue, filteredAdsType);
+  newMarkersInitMap(filteredMarkers);
 })
 
-// const housingPrice = document.querySelector('#housing-price');
-// const housingRooms = document.querySelector('#housing-rooms');
-// const housingGuests = document.querySelector('#housing-guests');
-// const housingFeatures = document.querySelector('#housing-features');
-// const housingFeaturesList = housingFeatures.querySelectorAll('input');
-// const checkedFeatures = () => {
-//   const checkedFeaturesArray = new Array;
-//   Array.from(housingFeaturesList).forEach((element) => {
-//     if(element.checked == true) {
-//       checkedFeaturesArray.push(element.value);
-//     }
-//   });
-//   return checkedFeaturesArray;
-// }
+// price filters
 
-// const adsArray = (offers) => {
+const pushFilteredElementPrice = (selectValue, filteredAdsPrice) => {
 
-// }
+  const getAdPriceValue = (element) => {
+    if (element.offer.price < 10000) {
+      return 'low';
+    } else if (element.offer.price >= 10000 && element.offer.price <= 50000) {
+      return 'middle';
+    } else if (element.offer.price > 50000) {
+      return 'high';
+    }
+  }
 
-// export {adsArray};
+  filteredAdsPrice.forEach((element) => {
+    if (selectValue === 'any') {
+      filteredAdsPrice.push(element);
+    }
+  })
+
+  if (selectValue === 'any') {
+    filteredAdsPrice.splice(0, filteredAdsPrice.length - 10);
+  } else {
+    for (let i = filteredAdsPrice.length - 1; i >= 0 ; i--) {
+      if (getAdPriceValue(filteredAdsPrice[i]) != selectValue) {
+        const indexOfEl = filteredAdsPrice.indexOf(filteredAdsPrice[i]);
+        filteredAdsPrice.splice(indexOfEl, 1);
+      }
+    }
+  }
+
+  filteredAdsPrice.forEach((element) => {
+    filteredMarkers.push(element);
+  })
+
+  console.log(filteredMarkers);
+};
+
+housingPrice.addEventListener('change', (offers) => {
+
+  let selectValue = housingPrice.value;
+
+  console.log(filteredAdsPrice);
+
+  if (filteredAdsPrice.length > 0) {
+    markersData(offers, filteredAdsPrice);
+    filteredMarkers.forEach((element) => {
+      filteredAdsPrice.push(element);
+    });
+  } else {
+    markersData(offers, filteredAdsPrice);
+  }
+
+  console.log(filteredAdsPrice);
+
+  for (let i = filteredAdsPrice.length -1 ; i > 0 ; i--) {
+    filteredAdsPrice.forEach((element) => {
+      if (filteredAdsPrice[i] != element) {
+        const indexOfEl = filteredMarkers.indexOf(filteredMarkers[i]);
+        filteredMarkers.splice(indexOfEl, 1);
+      }
+    })
+  }
+
+  pushFilteredElementPrice(selectValue, filteredAdsPrice);
+  initMap(filteredMarkers);
+
+  console.log(filteredMarkers);
+})
+
 
 // const getAdsRank = (ad) => {
 
@@ -95,3 +249,5 @@ housingType.addEventListener('change', (offers) => {
 
 //   return rank;
 // }
+
+
